12098e1e2c2f0b37450c52ffc69ff75704ba52c6
priznack <mangachka@mail.ru>
2025-05-17 21:17:56 +0300

    init a clicks.module

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-import './styles.css'
+import "./styles.css";
+import { ContextMenu } from "./menu.js";
+import { ClicksModule } from "./modules/clicks.module.js";
 
+// Заблокировать контекстное меню браузера
+document.addEventListener(
+  "contextmenu",
+  function (cancel) {
+    cancel.preventDefault();
+  },
+  false
+);
+
+const contextMenu = new ContextMenu(".menu"); // Создание нового экземпляра меню
+const clickFast = new ClicksModule("Считать клики (за 3 секунды)"); // Создание нового экземпляра модуля
+
+contextMenu.add(clickFast); // Добавляем модуль в меню
+console.log("Контекстное меню успешно создано!");
