99133689fa2c1f1c6d52afd71215773279d9383d
Dmitry Seregin <dimenerDS@gmail.com>
2025-05-17 14:02:08 +0400

    Сделано Меню по клику, элемент цвет фона

--- src/app.js
+++ src/app.js
@@ -1,2 +1,18 @@
-import './styles.css'
+import "./styles.css";
+import { ContextMenu } from "./menu.js";
+import { BackgroundModule } from "./modules/background.module.js";
 
+// Заблокировать контекстное меню браузера
+document.addEventListener(
+  "contextmenu",
+  function (cancel) {
+    cancel.preventDefault();
+  },
+  false
+);
+
+const contextMenu = new ContextMenu(".menu"); // Создание нового экземпляра меню
+const backgroundModule = new BackgroundModule("Изменить фон"); // Создание нового экземпляра модуля
+
+contextMenu.add(backgroundModule); // Добавляем модуль в меню
+console.log("Контекстное меню успешно создано!");
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,38 @@
-import {Menu} from './core/menu'
+import { Menu } from "./core/menu";
 
 export class ContextMenu extends Menu {
+  constructor(selector) {
+    super(selector);
+    this.el = document.querySelector(selector);
 
+    document.body.addEventListener("click", (event) => {
+      if (event.target.offsetParent !== this.el) {
+        this.close();
+      }
+    });
+
+    // Регистрируем обработчики событий
+    window.addEventListener("contextmenu", (event) => {
+      const x = event.clientX;
+      const y = event.clientY;
+      this.open(x, y);
+    });
+    // Закрываем меню при любом другом клике. Правый клик открывает новое меню, левый закрывает.
+    window.addEventListener("mousedown", () => this.close());
+  }
+
+  open(x, y) {
+    this.el.style.display = "block"; // Показываем меню
+    this.el.style.left = `${x}px`; // Устанавливаем координату X
+    this.el.style.top = `${y}px`; // Устанавливаем координату Y
+  }
+
+  close() {
+    this.el.style.display = "none"; // Скрываем меню
+  }
+
+  add(module) {
+    const liElement = module.toHTML(); // Получаем HTML представление модуля
+    this.el.innerHTML += liElement; // Добавляем новый пункт меню
+  }
 }
